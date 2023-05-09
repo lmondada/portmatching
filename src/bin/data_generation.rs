aa80e56272dec063c40d0836e4649bbcaf77a212
Luca Mondada <luca@mondada.net>
2023-05-09 12:03:09 +0200

    Datagen with trie serialization

--- src/bin/data_generation.rs
+++ src/bin/data_generation.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
-use portmatching::utils::is_connected;
+use portmatching::{
+    utils::is_connected, BalancedTrieMatcher, DetTrieMatcher, ManyPatternMatcher, NaiveManyMatcher,
+    NonDetTrieMatcher, Pattern,
+};
 use rmp_serde;
+use serde::Serialize;
 use std::fs;
 
 use portgraph::{Direction, PortGraph, PortIndex};
@@ -69,24 +73,79 @@ fn main() {
         }
     }
     // small graphs
-    {
+    let patterns = {
         fs::create_dir_all(format!("{dir}/small_graphs")).expect("could not create directory");
         let (n_graphs, n, m, d) = (args.n_small, args.v_small, args.e_small, args.d_small);
-        for i in 0..n_graphs {
-            println!("{}/{n_graphs} small graphs...", i + 1);
-            let mut g = None;
-            let mut n_fails = 0;
-            while g.is_none() || !is_connected(g.as_ref().unwrap()) {
-                g = gen_graph(n, m, d, &mut rng);
-                n_fails += 1;
-                if n_fails >= 10000 {
-                    panic!("could not create connected graph with n={n}, m={m}, d={d}")
+        (0..n_graphs)
+            .map(|i| {
+                println!("{}/{n_graphs} small graphs...", i + 1);
+                let mut g = None;
+                let mut n_fails = 0;
+                while g.is_none() || !is_connected(g.as_ref().unwrap()) {
+                    g = gen_graph(n, m, d, &mut rng);
+                    n_fails += 1;
+                    if n_fails >= 10000 {
+                        panic!("could not create connected graph with n={n}, m={m}, d={d}")
+                    }
                 }
-            }
-            let g = g.unwrap();
-            let f = format!("{dir}/small_graphs/pattern_{i}.bin");
-            fs::write(f, rmp_serde::to_vec(&g).unwrap()).expect("could not write to file");
-        }
+                let g = g.unwrap();
+                let f = format!("{dir}/small_graphs/pattern_{i}.bin");
+                fs::write(f, rmp_serde::to_vec(&g).unwrap()).expect("could not write to file");
+                g
+            })
+            .collect::<Vec<_>>()
+    };
+
+    println!("Precompute graph tries");
+    let dir = format!("{dir}/tries");
+    fs::create_dir_all(&dir).expect("could not create directory");
+    construct_tries(patterns, &dir);
+}
+
+fn construct_tries(patterns: Vec<PortGraph>, dir: &str) {
+    let patterns = patterns
+        .into_iter()
+        .map(|g| Pattern::from_graph(g).unwrap())
+        .collect::<Vec<_>>();
+    construct_trie(
+        BalancedTrieMatcher::from_patterns,
+        &patterns,
+        "balanced_trie",
+        dir,
+    );
+    construct_trie(
+        |ps| BalancedTrieMatcher::from_patterns(ps).to_cached_trie(),
+        &patterns,
+        "balanced_trie_cached",
+        dir,
+    );
+    construct_trie(DetTrieMatcher::from_patterns, &patterns, "det_trie", dir);
+    construct_trie(
+        NonDetTrieMatcher::from_patterns,
+        &patterns,
+        "non_det_trie",
+        dir,
+    );
+    construct_trie(
+        NaiveManyMatcher::from_patterns,
+        &patterns,
+        "naive_matcher",
+        dir,
+    );
+}
+
+fn construct_trie<F: Fn(Vec<Pattern>) -> T, T: Serialize>(
+    builder: F,
+    patterns: &Vec<Pattern>,
+    name: &str,
+    dir: &str,
+) {
+    for i in (0..patterns.len()).step_by(100) {
+        let patterns = patterns[0..i].to_vec();
+        println!("Constructing {} for {} patterns", name, i);
+        let matcher = builder(patterns);
+        let f = format!("{dir}/tries/{name}_{i}.bin");
+        fs::write(f, rmp_serde::to_vec(&matcher).unwrap()).expect("could not write to file");
     }
 }
 
