2c5bdc7b2bf7e9311869764cd61e9af29c949ec5
Luca Mondada <luca@mondada.net>
2023-07-06 15:34:54 +0200

    Fixing bugs

--- examples/large_graph.rs
+++ examples/large_graph.rs
@@ -1,21 +1,20 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, hint::black_box};
 
 use portgraph::PortGraph;
-use portmatching::{matcher::ManyMatcher, Pattern, PortMatcher};
+use portmatching::{matcher::UnweightedManyMatcher, PortMatcher};
 
 fn main() {
-    let path: PathBuf = ["examples", "data"].iter().collect();
-    let mut patterns = Vec::with_capacity(100);
-    for i in 0..100 {
-        let path = path.join(format!("small_circuits/pattern_{}.json", i));
-        let p: PortGraph = serde_json::from_reader(fs::File::open(&path).unwrap()).unwrap();
-        patterns.push(Pattern::from_portgraph(&p));
-    }
+    let path: PathBuf = ["datasets", "xxl"].iter().collect();
+
+    let file_name = path.join(&format!("tries/balanced_1000.bin"));
+    let matcher: UnweightedManyMatcher = rmp_serde::from_read(fs::File::open(file_name).unwrap())
+        .expect("could not deserialize trie");
+
     let path = path.join("large_circuits/circuit_0.json");
     let graph: PortGraph = serde_json::from_reader(fs::File::open(path).unwrap()).unwrap();
 
     println!("Loaded graph and patterns");
-    let matcher = ManyMatcher::from_patterns(patterns);
-    println!("Built matcher");
-    matcher.find_matches(&graph);
+    for _ in 0..5 {
+        black_box(matcher.find_matches(&graph));
+    }
 }
