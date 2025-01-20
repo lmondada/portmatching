2bfbc2ac157a1d6c6ab120ee1658418d75789e3f
Luca Mondada <luca@mondada.net>
2025-01-20 09:16:12 +0100

    Bump portgraph to v0.13

--- src/utils/test.rs
+++ src/utils/test.rs
@@ -31,30 +31,8 @@ impl<S: BindMap<Key = PGIndexKey, Value = NodeIndex>> From<PatternMatch<S>> for 
     }
 }
 
-// pub(crate) fn graph() -> PortGraph {
-//     let mut g = PortGraph::new();
-//     let v0 = g.add_node(2, 2);
-//     let v1 = g.add_node(2, 3);
-//     let vlol = g.add_node(3, 4);
-//     let v2 = g.add_node(2, 1);
-//     let v3 = g.add_node(2, 2);
-//     let v0_out0 = g.port_index(v0, PortOffset::new_outgoing(0)).unwrap();
-//     let v1_out1 = g.port_index(v1, PortOffset::new_outgoing(1)).unwrap();
-//     let v2_in0 = g.port_index(v2, PortOffset::new_incoming(0)).unwrap();
-//     let v2_in1 = g.port_index(v2, PortOffset::new_incoming(1)).unwrap();
-//     let v2_out0 = g.port_index(v2, PortOffset::new_outgoing(0)).unwrap();
-//     let v3_in1 = g.port_index(v3, PortOffset::new_incoming(1)).unwrap();
-//     g.link_ports(v0_out0, v2_in1).unwrap();
-//     g.link_ports(v1_out1, v2_in0).unwrap();
-//     g.link_ports(v2_out0, v3_in1).unwrap();
-//     g.remove_node(vlol);
-//     g
-// }
-
-#[cfg(feature = "proptest")]
 pub use self::proptests::*;
 
-#[cfg(feature = "proptest")]
 mod proptests {
     use super::*;
     use portgraph::proptest::{gen_multiportgraph, gen_portgraph};
